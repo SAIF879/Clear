1c961004f505a69bb99d7dffddfb5df6ecdbad6a
SAIF879 <saifahmed9308@gmail.com>
2023-06-22 18:10:04 +0530

    created dao , database

--- app/src/main/java/com/example/clear/di/AppModule.kt
+++ app/src/main/java/com/example/clear/di/AppModule.kt
@@ -1,11 +1,27 @@
 package com.example.clear.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.clear.room.dao.NoteDataBaseDao
+import com.example.clear.room.database.NoteDataBase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 //modules are used to add bindings to hilt
 @InstallIn(SingletonComponent::class) // this will not be recreated , just one instance
 @Module
 object AppModule {
+    @Singleton
+    @Provides
+    fun provideNotesDao(noteDataBase: NoteDataBase): NoteDataBaseDao = noteDataBase.noteDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDataBase(@ApplicationContext context: Context): NoteDataBase =
+        Room.databaseBuilder(context, NoteDataBase::class.java, "notes_db")
+            .fallbackToDestructiveMigration().build()
 }
--- app/src/main/java/com/example/clear/room/dao/NoteDataBaseDao.kt
+++ app/src/main/java/com/example/clear/room/dao/NoteDataBaseDao.kt
@@ -1,32 +1,34 @@
 package com.example.clear.room.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.clear.room.model.Note
-import retrofit2.http.DELETE
+import kotlinx.coroutines.flow.Flow
 
 //dao access sqlite data
 @Dao
 interface NoteDataBaseDao {
 
     @Query("SELECT * from note_tbl")
-    fun getNotes() : List<Note>
+    fun getNotes(): Flow<List<Note>>
 
     @Query("SELECT * from note_tbl where id =:id")
-    fun getNoteById(id:String) :Note
+    suspend fun getNoteById(id: String): Note
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertNote(note:Note)
+    suspend fun insertNote(note: Note)
 
-    @Update(onConflict =  OnConflictStrategy.REPLACE)
-    fun updateNote(note:Note)
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateNote(note: Note)
 
-   @Query("DELETE from note_tbl")
-   fun clearNotes()
+    @Query("DELETE from note_tbl")
+    suspend fun clearNotes()
 
-   @DELETE
-   fun deleteNote(note : Note)
+    @Delete
+    suspend fun deleteNote(note: Note)
+
 }
