6e7a5c1c7ff08c6c03fdaa8f91e7fe5c2a14b9c9
SAIF879 <saifahmed9308@gmail.com>
2023-06-23 19:30:07 +0530

    added in the fonts for app

--- app/src/main/java/com/example/clear/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/clear/screens/splash/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.clear.screens.splash
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,20 +10,50 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph
 import com.example.clear.navigation.NavGraphs
+import com.example.clear.ui.theme.DeepBlue
+import com.example.clear.ui.theme.LightRed
 import com.example.clear.utils.commonComponents.GenerateButton
+import com.example.clear.utils.fonts.FontFamilyClear
 
+@Preview
 @Composable
-fun SplashScreen(navController: NavController) {
-    Box(modifier = Modifier.fillMaxSize()){
-        Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally , modifier = Modifier.fillMaxSize()){
-            Text(text = "Splash Screen" , color = Color.Black , fontSize = 30.sp)
-            GenerateButton{
-                navController.navigate(NavGraphs.Introduction)
-            }
+fun SplashScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(DeepBlue)){
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+
+        }
+
         }
     }
+
+@Composable
+fun ShowAnimation(){
+
+}
+
+@Composable
+fun ShowHeading(heading: String, fontSize: Int) {
+    Text(
+        text = "",
+        style = TextStyle(
+            fontSize = fontSize.sp,
+            color = LightRed,
+            textAlign = TextAlign.Center,
+            fontFamily =FontFamilyClear.fontMedium
+        )
+    )
 }
---
+++ app/src/main/java/com/example/clear/utils/fonts/Fonts.kt
@@ -0,0 +1,14 @@
+package com.example.clear.utils.fonts
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.clear.R
+
+object FontFamilyClear {
+    val fontBlack = FontFamily(Font(R.font.cinzel_black))
+    val fontBold = FontFamily(Font(R.font.cinzel_bold))
+    val fontExtraBold = FontFamily(Font(R.font.cinzel_extrabold))
+    val fontMedium = FontFamily(Font(R.font.cinzel_medium))
+    val fontRegular = FontFamily(Font(R.font.cinzel_regular))
+    val fontSemiBold = FontFamily(Font(R.font.cinzel_semibold))
+}
