34d54ce1f0b2e0eb38fd631d2cc3ff1de68722c7
SAIF879 <saifahmed9308@gmail.com>
2023-06-25 00:40:42 +0530

    upgraded the bottom navigation , though ui needed upgrade , bottom bar visible till specified roots

--- app/src/main/java/com/example/clear/screens/bottomBar/BottomBarScreen.kt
+++ app/src/main/java/com/example/clear/screens/bottomBar/BottomBarScreen.kt
@@ -1,29 +1,42 @@
 package com.example.clear.screens.bottomBar
 
+import android.media.Image
+import androidx.annotation.DrawableRes
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Checklist
+import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.GraphicEq
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.SurroundSound
+import androidx.compose.material.icons.outlined.Checklist
+import androidx.compose.material.icons.outlined.Description
+import androidx.compose.material.icons.outlined.GraphicEq
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomBarScreen(
     val route: String,
     val title: String,
-    val icon: ImageVector
+     val iconFilled : ImageVector ,
+     val iconOutlined :ImageVector,
+
 ){
     object Notes : BottomBarScreen(
         route =  "notes",
         title = "Notes",
-        icon = Icons.Default.Home
+        iconFilled = Icons.Filled.Description,
+        iconOutlined = Icons.Outlined.Description
     )
     object Todos : BottomBarScreen(
         route = "todos",
         title = "To-do",
-        icon = Icons.Default.Done
+        iconFilled = Icons.Filled.Checklist,
+        iconOutlined =Icons.Outlined.Checklist
     )
     object Memos : BottomBarScreen(
         route = "memos",
         title = "Voice-Memo",
-        icon = Icons.Default.SurroundSound
+       iconFilled =  Icons.Filled.GraphicEq,
+        iconOutlined = Icons.Outlined.GraphicEq
     )
 }
--- app/src/main/java/com/example/clear/screens/bottomBar/HomeScreen.kt
+++ app/src/main/java/com/example/clear/screens/bottomBar/HomeScreen.kt
@@ -1,7 +1,13 @@
 package com.example.clear.screens.bottomBar
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -9,10 +15,16 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.ProvidedValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -20,51 +32,110 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.clear.navigation.BottomNavGraph
-import com.example.clear.ui.theme.DeepBlue
-import com.example.clear.ui.theme.LightRed
 import com.example.clear.ui.theme.TextWhite
+import com.example.clear.utils.fonts.FontFamilyClear
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(){
     val navController = rememberNavController()
-    Scaffold(bottomBar = { BottomBar(navController = navController)}) {
+    val showBottomBar = rememberSaveable {
+        mutableStateOf(true)
+    }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    when(navBackStackEntry?.destination?.route){
+        "notes" -> showBottomBar.value = true
+        "todos" ->showBottomBar.value = true
+        "memos" -> showBottomBar.value = true
+        else -> showBottomBar.value = false
+    }
+
+    Scaffold(bottomBar = { BottomBar(navController = navController , bottomBarState = showBottomBar)}) {
         BottomNavGraph(navController = navController)
     }
 
+
 }
+
 @Composable
-fun BottomBar(navController: NavHostController){
+fun BottomBar(navController: NavHostController, bottomBarState: MutableState<Boolean>){
 val screens = listOf(
     BottomBarScreen.Notes,
     BottomBarScreen.Todos,
     BottomBarScreen.Memos
 )
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
 
-    NavigationBar(containerColor = TextWhite) {
-        screens.forEach{screen->
-        AddItem(screen = screen, currentDestination = currentDestination , navController =navController )
+
+    AnimatedVisibility(
+        visible = bottomBarState.value,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically(targetOffsetY = {it}),
+        content = {
+            BottomBarScreenContent(navController = navController , bottomBarState  = bottomBarState, screens = screens)
         }
-    }
+    )
+}
 
+private object NoRippleTheme : RippleTheme {
+    @Composable
+    override fun defaultColor() = Color.Unspecified
+
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = RippleAlpha(0.0f,0.0f,0.0f,0.0f)
 }
 
 @Composable
-fun RowScope.AddItem(
-    screen: BottomBarScreen,
-    currentDestination: NavDestination?,
-    navController: NavHostController
+fun BottomBarScreenContent(
+    navController: NavHostController,
+    bottomBarState: MutableState<Boolean>,
+    screens: List<BottomBarScreen>
 ) {
-    NavigationBarItem(label = { Text(text = screen.title) },
-        icon = { Icon(imageVector = screen.icon, contentDescription = "bottom_icon") },
-        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-        onClick = { navController.navigate(screen.route){
-            popUpTo(navController.graph.findStartDestination().id)
-            launchSingleTop = true
-        } } ,
-        )
+    CompositionLocalProvider() {
+        NavigationBar() {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
 
+            screens.forEach { menuItem ->
+                val selected =
+                    currentDestination?.hierarchy?.any { it.route == menuItem.route } == true
+
+                // adding each item
+                NavigationBarItem(
+                    selected = (selected),
+                    onClick = {
+                        if (selected) return@NavigationBarItem
+                        navController.navigate(menuItem.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = if (selected) menuItem.iconFilled else menuItem.iconOutlined,
+                            contentDescription = menuItem.route
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = menuItem.title,
+                            style = TextStyle(
+                                fontFamily = if (selected) FontFamilyClear.fontMedium else FontFamilyClear.fontRegular,
+                                fontSize = 14.sp
+                            )
+                        )
+                    },
+                    enabled = true
+                )
+            }
+        }
+    }
 }
+
+
+
+
